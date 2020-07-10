880aab1476cae2c75a8ce6604d8ffa6e1c2943da
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2020-07-10 18:31:22 -0500

    add graphics to prototype

--- Assets/Scripts/Checkbox.cs
+++ Assets/Scripts/Checkbox.cs
@@ -9,8 +9,13 @@ public class Checkbox : MonoBehaviour
 
     public bool isChecked;
 
+
+    private void Start() {
+        checkSr.color = checkSr.color.WithAlpha(0);
+    }
+
     public void OnChecked() {
         isChecked = true;
-        checkSr.color = checkSr.color.WithAlpha(0);
+        checkSr.color = checkSr.color.WithAlpha(1);
     }
 }
