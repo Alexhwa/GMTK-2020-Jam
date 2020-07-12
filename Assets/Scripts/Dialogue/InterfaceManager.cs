0cc77f823436d58f2c401d1df2925244589bcf6b
Alexhwa <ah.alswad@gmail.com>
2020-07-11 21:58:49 -0500

    Improved dialogue voice

--- Assets/Scripts/Dialogue/InterfaceManager.cs
+++ Assets/Scripts/Dialogue/InterfaceManager.cs
@@ -149,7 +149,7 @@ public class InterfaceManager : MonoBehaviour
         }
         else
         {
-            if (Random.Range(0, 1.0f) < .3f)
+            if (Random.Range(0, 1.0f) < .15f)
             {
                 var chosenClip = currentDialogue.character.charVoice[Random.Range(0, currentDialogue.character.charVoice.Length)];
                 audioManager.PlayOneShot(chosenClip);
